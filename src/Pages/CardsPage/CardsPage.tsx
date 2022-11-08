776fd747690a523145251fa537f6ce81891febc8
aivan1 <78039586+aivan1@users.noreply.github.com>
2022-11-08 22:40:06 +0200

    style changes

--- src/Pages/CardsPage/CardsPage.tsx
+++ src/Pages/CardsPage/CardsPage.tsx
@@ -1,11 +1,19 @@
 import { Card } from "../../Components/Card/Card";
 import { NavBar } from "../../Components/NavBar/NavBar";
+import { useColorTheme } from "../../Context/theme-context";
+import styles from "./CardsPage.module.scss";
 
 export const CardsPage = () => {
+  const theme = useColorTheme();
+  console.log(theme.theme);
   return (
-    <>
+    <div
+      className={`${styles.container} ${
+        theme.theme === "dark" ? styles.dark : null
+      }`}
+    >
       <NavBar />
-      {/* <Card /> */}
-    </>
+      <Card />
+    </div>
   );
 };
