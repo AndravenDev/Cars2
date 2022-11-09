719972df0461032a159c2ab4560f5f1577e3b97d
aivan1 <78039586+aivan1@users.noreply.github.com>
2022-11-09 12:51:39 +0200

    Added more styling to the cards

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -11,15 +11,24 @@ export const Card = () => {
     >
       <div className={styles.picture}></div>
       <div className={styles.keyInfo}>
-        <h1>BMW</h1>
-        <p>16 000</p>
+        <p className={styles.title}>Mercedes E350 4Matic Facelift Navi</p>
+        <p
+          className={`${styles.price} ${
+            theme.theme === "dark" ? styles.darkPrice : "light"
+          }`}
+        >
+          16 000 лв
+        </p>
       </div>
+      <p className={styles.fuelYearAndMileage}>2009, Diesel 210 000 km</p>
       <p className={styles.description}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quaerat
-        numquam, quod saepe hic tempore. Optio cupiditate facilis porro nobis
-        velit corporis aperiam, sint vitae architecto, delectus fugiat sequi
-        neque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At sit
+        voluptate aspernatur totam assumenda quod velit quo facilis vitae
+        excepturi quos voluptas officiis molestias, dolorem rem, aut ad
+        reiciendis dolores.
       </p>
+      <p className={styles.publisherInfo}>Private seller, Sofia</p>
     </div>
   );
 };
