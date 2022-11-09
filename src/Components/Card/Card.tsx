c559cafdf381adc30861c149cbe556ae5384e420
aivan1 <78039586+aivan1@users.noreply.github.com>
2022-11-09 16:31:01 +0200

    added courosel

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,15 +1,51 @@
 import styles from "./Card.module.scss";
 import { useColorTheme } from "../../Context/theme-context";
+import { useState } from "react";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 
 export const Card = () => {
+  const images = [
+    "https://c4.wallpaperflare.com/wallpaper/537/50/352/mercedes-benz-amg-black-vossen-wallpaper-preview.jpg",
+    "https://wallpapercave.com/wp/wp7896711.jpg",
+    "https://p4.wallpaperbetter.com/wallpaper/299/78/483/mercedes-benz-amg-black-vossen-wallpaper-preview.jpg",
+    "https://c4.wallpaperflare.com/wallpaper/87/707/680/mercedes-e55-amg-hd-wallpaper-preview.jpg",
+    "https://c4.wallpaperflare.com/wallpaper/1020/132/850/mercedes-benz-amg-e55-hd-wallpaper-preview.jpg",
+  ];
   const theme = useColorTheme();
+  const [imageIndex, setImageIndex] = useState(0);
+  const shiftImages = (n: number) => {
+    let result = imageIndex + n;
+    if (result === images.length) {
+      result = 0;
+    } else if (result < 0) {
+      result = images.length - 1;
+    }
+
+    setImageIndex(result);
+  };
   return (
     <div
       className={`${styles.container} ${
         theme.theme === "dark" ? styles.dark : "light"
       }`}
     >
-      <div className={styles.picture}></div>
+      {/* <div className={styles.picture}></div> */}
+      <div className={styles.caurosel}>
+        <img src={images[imageIndex]} />
+        <ChevronLeftIcon
+          className={styles.prev}
+          onClick={() => shiftImages(-1)}
+        >
+          prev
+        </ChevronLeftIcon>
+        <ChevronRightIcon
+          className={styles.next}
+          onClick={() => shiftImages(1)}
+        >
+          next
+        </ChevronRightIcon>
+      </div>
       <div className={styles.keyInfo}>
         <p className={styles.title}>Mercedes E350 4Matic Facelift Navi</p>
         <p
