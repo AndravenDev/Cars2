d4048b9bc0114b8190f29bdf21d7662040cb07d0
aivan1 <78039586+aivan1@users.noreply.github.com>
2022-11-11 13:41:45 +0200

    added basic structure for filters

--- src/Components/Filters/Filters.tsx
+++ src/Components/Filters/Filters.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
 import Chip from "@mui/material/Chip";
 import styles from "./Filters.module.scss";
 import { useColorTheme } from "../../Context/theme-context";
 import { Button } from "@mui/material";
+import { useState } from "react";
 
 export const Filters = () => {
   const theme = useColorTheme();
@@ -24,16 +24,61 @@ export const Filters = () => {
     "Published",
     "Condition",
   ];
+  const [currentFilter, setCurrentFilter] = useState<any>();
+
+  const getCurrentFilterUI = (key: string) => {
+    const filterUIs = {
+      coupe: () => {
+        const coupeTypes = ["Wagon", "Sendan", "Hatchback"];
+        return (
+          <div>
+            {coupeTypes.map((x, i) => (
+              <Chip
+                key={i}
+                className={`${styles.chip} ${styles.openFilterChip}`}
+                label={x}
+                variant="outlined"
+                color="primary"
+              />
+            ))}
+          </div>
+        );
+      },
+    };
+    if (key && Object.keys(filterUIs).includes(key)) {
+      return filterUIs[key as keyof typeof filterUIs];
+    }
+    return () => <div></div>;
+  };
+  console.log(getCurrentFilterUI("coupe"));
   return (
     <div
       className={`${styles.container} ${
         theme.theme === "dark" ? styles.dark : null
       }`}
     >
-      {filters.map((x) => (
-        <Chip className={styles.chip} label={x} onClick={() => {}} />
+      {filters.map((x, i) => (
+        <Chip
+          key={i}
+          className={styles.chip}
+          label={x}
+          onClick={() => {
+            setCurrentFilter(x);
+          }}
+        />
       ))}
-      <Button className={styles.clearButton}>Clear</Button>
+      <Button
+        className={styles.clearButton}
+        onClick={() => setCurrentFilter(null)}
+      >
+        Clear
+      </Button>
+
+      <div className={styles.openFilter}>
+        {currentFilter
+          ? getCurrentFilterUI(currentFilter.toLowerCase())()
+          : null}
+      </div>
     </div>
   );
 };
